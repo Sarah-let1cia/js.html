2e103c93f475da3d748a0cd2fdd6c59642f2f598
Sarah Letícia <sarah.lettsofi@gmail.com>
2026-02-25 14:26:01 -0300

    Add files via upload

---
+++ cod3.js
@@ -0,0 +1,17 @@
+//repetiçao
+//for
+
+for (let i =0; i<10; i++){
+    console.log(i);
+}
+
+//while
+
+let cont = 3;
+while(cont >= )
+{
+    console.log ("contador: " + cont);
+    cont--; //cont = cont - 1; 
+    //*se quise tirar mais usar use d tal forma export. cont -=3*//
+
+}
