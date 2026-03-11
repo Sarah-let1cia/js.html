3e0d0ccb0d093af4b64988f47a8e9ffd3069fe48
Sarah Letícia <sarah.lettsofi@gmail.com>
2026-03-11 16:39:20 -0300

    Add files via upload

---
+++ shj.js
@@ -0,0 +1,15 @@
+let h1 = document.getElementById("titulo");
+console.log(h1);
+
+h1.textContent = "2 infonet";
+h1.style.color = "green";
+
+let p = document.querySelector(".paragrafo");
+console.log(p);
+
+p.textContent = "teste";
+
+let div = document.querySelector(".fundo");
+
+div.style.color = "red";
+
